b8546afa77c9074dcb67d6e6172d81c40b9bdbf7
MartinEricsson234 <martinkruge@hotmail.com>
2024-01-06 12:12:38 +0000

    Added a client and server directory split inside the same repository

---
+++ client/src/App.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+
+function Login() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        // Send a POST request to the backend
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input
+                type="text"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                placeholder="Username"
+            />
+            <input
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="Password"
+            />
+            <button type="submit">Login</button>
+        </form>
+    );
+}
+
+export default Login;
